2f5c5a33eed006181464abf22ae847a745ae2ddc
kristianrecio <kristianrec@gmail.com>
2017-06-24 10:36:43 -0500

    Simple connection code added to Server.py

--- Server.py
+++ Server.py
@@ -1,4 +1,17 @@
 # Server Class
 
+import socket
+
+
 class Server:
-    pass
+    s = socket.socket()
+    host = socket.gethostname()
+    port = 1234
+    s.bind((host, port))
+
+    s.listen(5)
+    while True:
+        c, addr = s.accept()
+        print("Got connection from", addr)
+        c.send(b"Thank you for connecting")
+        c.close()
