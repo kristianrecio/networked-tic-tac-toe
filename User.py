2e72df4266661091389993d1844b3726d87feac9
Nathanael <scorpioncarr1@gmail.com>
2017-06-30 14:47:32 -0500

    I added the buttons for the user to play the game. More coming soon...

--- User.py
+++ User.py
@@ -1,10 +1,41 @@
-import tkinter
+import tkinter as tk
+from tkinter import *
 
-def main():
+def toggle_text1(button):
+    if button['text'] == "":
+        button['text'] = "X"
+    elif button['text'] == "X":
+        button['text'] = "O"
+    else:
+        button['text'] = ""
 
-    #Creates the main window
-    main_window = tkinter.Tk()
+root = tk.Tk()
+root.title("Network Tic-Tac-Toe")
+button1 = tk.Button(text="", width=5, command= lambda : toggle_text1(button1))
+button1.grid(row=1, column=1, padx=5, pady=5, sticky=E)
 
-    tkinter.mainloop()
+button2 = tk.Button(text="", width=5, command= lambda : toggle_text1(button2))
+button2.grid(row=1, column=2, padx=5, pady=5, sticky=E)
 
-main()
+button3 = tk.Button(text="", width=5, command= lambda : toggle_text1(button3))
+button3.grid(row=1, column=3, padx=5, pady=5, sticky=E)
+
+button4 = tk.Button(text="", width=5, command= lambda : toggle_text1(button4))
+button4.grid(row=2, column=1, padx=5, pady=5, sticky=E)
+
+button5 = tk.Button(text="", width=5, command= lambda : toggle_text1(button5))
+button5.grid(row=2, column=2, padx=5, pady=5, sticky=E)
+
+button6 = tk.Button(text="", width=5, command= lambda : toggle_text1(button6))
+button6.grid(row=2, column=3, padx=5, pady=5, sticky=E)
+
+button7 = tk.Button(text="", width=5, command= lambda : toggle_text1(button7))
+button7.grid(row=3, column=1, padx=5, pady=5, sticky=E)
+
+button8 = tk.Button(text="", width=5, command= lambda : toggle_text1(button8))
+button8.grid(row=3, column=2, padx=5, pady=5, sticky=E)
+
+button9 = tk.Button(text="", width=5, command= lambda : toggle_text1(button9))
+button9.grid(row=3, column=3, padx=5, pady=5, sticky=E)
+
+root.mainloop()
