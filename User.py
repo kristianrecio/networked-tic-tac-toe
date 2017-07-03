deb781ae3cda1e19dd358a20f63cb712b0b69310
Nathanael <scorpioncarr1@gmail.com>
2017-07-03 15:27:39 -0500

    Sebastian moved the user.py functions into classes.

--- User.py
+++ User.py
@@ -1,47 +1,63 @@
 import tkinter as tk
 
-def toggle_text1(button):  # This function changes the blank button to what it needs to be
-    if button['text'] == "":
-        button['text'] = "X"
-    elif button['text'] == "X":
-        button['text'] = "O"
-    else:
-        button['text'] = ""
 
-#  1) Change the color of the buttons on each click
-#  2) Create a black bar to mark out the game lines
-#  3) Change the font size
+class User:
 
-root = tk.Tk()
-root.title("Network Tic-Tac-Toe")
-root.geometry('{}x{}'.format(300, 250))  # Dimensions of the GUI
+    def __init__(self):
 
-# These are the buttons for the game
-button1 = tk.Button(text="", width=5, height=2, command=lambda: toggle_text1(button1))
-button1.place(relx=.3, rely=.2, anchor="center")
+        self.root = tk.Tk()
+        self.root.title("Network Tic-Tac-Toe")
+        self.root.geometry('{}x{}'.format(300, 250))  # Dimensions of the GUI
 
-button2 = tk.Button(text="", width=5, height=2, command=lambda: toggle_text1(button2))
-button2.place(relx=.5, rely=.2, anchor="center")
+        self.buttons()
 
-button3 = tk.Button(text="", width=5, height=2, command=lambda: toggle_text1(button3))
-button3.place(relx=.7, rely=.2, anchor="center")
 
-button4 = tk.Button(text="", width=5, height=2, command=lambda: toggle_text1(button4))
-button4.place(relx=.3, rely=.4, anchor="center")
+    def toggle_text1(self, button):  # This function changes the blank button to what it needs to be
 
-button5 = tk.Button(text="", width=5, height=2, command=lambda: toggle_text1(button5))
-button5.place(relx=.5, rely=.4, anchor="center")
+        if button['text'] == "":
+            button['text'] = "X"
+        elif button['text'] == "X":
+            button['text'] = "O"
+        else:
+            button['text'] = ""
 
-button6 = tk.Button(text="", width=5, height=2, command=lambda: toggle_text1(button6))
-button6.place(relx=.7, rely=.4, anchor="center")
+    def buttons(self):
+        # These are the buttons for the game
+        self.button1 = tk.Button(text="", width=5, height=2, command=lambda: self.toggle_text1(self.button1))
+        self.button1.place(relx=.3, rely=.2, anchor="center")
 
-button7 = tk.Button(text="", width=5, height=2, command=lambda: toggle_text1(button7))
-button7.place(relx=.3, rely=.6, anchor="center")
+        self.button2 = tk.Button(text="", width=5, height=2, command=lambda: self.toggle_text1(self.button2))
+        self.button2.place(relx=.5, rely=.2, anchor="center")
 
-button8 = tk.Button(text="", width=5, height=2, command=lambda: toggle_text1(button8))
-button8.place(relx=.5, rely=.6, anchor="center")
+        self.button3 = tk.Button(text="", width=5, height=2, command=lambda: self.toggle_text1(self.button3))
+        self.button3.place(relx=.7, rely=.2, anchor="center")
 
-button9 = tk.Button(text="", width=5, height=2, command=lambda: toggle_text1(button9))
-button9.place(relx=.7, rely=.6, anchor="center")
+        self.button4 = tk.Button(text="", width=5, height=2, command=lambda: self.toggle_text1(self.button4))
+        self.button4.place(relx=.3, rely=.4, anchor="center")
 
-root.mainloop()
+        self.button5 = tk.Button(text="", width=5, height=2, command=lambda: self.toggle_text1(self.button5))
+        self.button5.place(relx=.5, rely=.4, anchor="center")
+
+        self.button6 = tk.Button(text="", width=5, height=2, command=lambda: self.toggle_text1(self.button6))
+        self.button6.place(relx=.7, rely=.4, anchor="center")
+
+        self.button7 = tk.Button(text="", width=5, height=2, command=lambda: self.toggle_text1(self.button7))
+        self.button7.place(relx=.3, rely=.6, anchor="center")
+
+        self.button8 = tk.Button(text="", width=5, height=2, command=lambda: self.toggle_text1(self.button8))
+        self.button8.place(relx=.5, rely=.6, anchor="center")
+
+        self.button9 = tk.Button(text="", width=5, height=2, command=lambda: self.toggle_text1(self.button9))
+        self.button9.place(relx=.7, rely=.6, anchor="center")
+
+        self.root.mainloop()
+
+
+
+                #  1) Change the color of the buttons on each click
+                #  2) Create a black bar to mark out the game lines
+                #  3) Change the font size
+
+User()
+
+
