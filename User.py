62096d5760629c3ebf40ba852b64e40be323a487
Nathanael <scorpioncarr1@gmail.com>
2017-07-04 17:00:53 -0500

    I figured out what was going on with the User() frame and the buttons!!
    I was missing the "self" on the button functions.

--- User.py
+++ User.py
@@ -62,38 +62,38 @@ class User(tk.Frame):  # This is the user windows
             else:
                 button['text'] = ""
 
-        def buttons(): # These are the buttons for the game
-            button1 = tk.Button(text="", width=5, height=2, command=lambda: controller.toggle_text1(button1))
+        def buttons(self):  # These are the buttons for the game
+            button1 = tk.Button(self, text="", width=5, height=2, command=lambda: toggle_text1(button1))
             button1.place(relx=.3, rely=.2, anchor="center")
 
-            button2 = tk.Button(text="", width=5, height=2, command=lambda: controller.toggle_text1(button2))
+            button2 = tk.Button(self, text="", width=5, height=2, command=lambda: toggle_text1(button2))
             button2.place(relx=.5, rely=.2, anchor="center")
 
-            button3 = tk.Button(text="", width=5, height=2, command=lambda: controller.toggle_text1(button3))
+            button3 = tk.Button(self, text="", width=5, height=2, command=lambda: toggle_text1(button3))
             button3.place(relx=.7, rely=.2, anchor="center")
 
-            button4 = tk.Button(text="", width=5, height=2, command=lambda: controller.toggle_text1(button4))
+            button4 = tk.Button(self, text="", width=5, height=2, command=lambda: toggle_text1(button4))
             button4.place(relx=.3, rely=.4, anchor="center")
 
-            button5 = tk.Button(text="", width=5, height=2, command=lambda: controller.toggle_text1(button5))
+            button5 = tk.Button(self, text="", width=5, height=2, command=lambda: toggle_text1(button5))
             button5.place(relx=.5, rely=.4, anchor="center")
 
-            button6 = tk.Button(text="", width=5, height=2, command=lambda: controller.toggle_text1(button6))
+            button6 = tk.Button(self, text="", width=5, height=2, command=lambda: toggle_text1(button6))
             button6.place(relx=.7, rely=.4, anchor="center")
 
-            button7 = tk.Button(text="", width=5, height=2, command=lambda: controller.toggle_text1(button7))
+            button7 = tk.Button(self, text="", width=5, height=2, command=lambda: toggle_text1(button7))
             button7.place(relx=.3, rely=.6, anchor="center")
 
-            button8 = tk.Button(text="", width=5, height=2, command=lambda: controller.toggle_text1(button8))
+            button8 = tk.Button(self, text="", width=5, height=2, command=lambda: toggle_text1(button8))
             button8.place(relx=.5, rely=.6, anchor="center")
 
-            button9 = tk.Button(text="", width=5, height=2, command=lambda: controller.toggle_text1(button9))
+            button9 = tk.Button(self, text="", width=5, height=2, command=lambda: toggle_text1(button9))
             button9.place(relx=.7, rely=.6, anchor="center")
 
             submit = tk.Button(self, text="Submit", command=lambda: controller.show_frame("Wait"))
             submit.place(relx=.5, rely=.8, anchor="center")  # This button goes to the Wait frame
 
-        buttons()
+        buttons(self)
 
 
 class Wait(tk.Frame):  # This window waits for the other player to submit
